faa857671669bcfef08325c993e607df0055d90c
Kanishka <kanishkamakhija007@gmail.com>
2017-10-04 20:10:13 +0530

    Update: python file names

--- PYTHON/linklist.py
+++ PYTHON/linklist.py
@@ -1,12 +1,19 @@
+#Class to create a node with key and next pointer
 class Node:
+
+#Constructor to initialize key & node    
     def __init__(self, data=None, next=None):
         self.data = data
         self.next = None
 
+#Class contains methods to perform operation on linklist
 class LinkList:
+
+#Constructor of the class to initialize head node
     def __init__(self):
         self.head = None
 
+#Method to add node at the beginning of linllist
     def add_beg(self, data):
         node = Node(data)
         if self.head == None:
@@ -15,6 +22,7 @@ class LinkList:
             node.next = self.head
             self.head = node
 
+#Method to add node at the end of linklist
     def add_end(self, data):
         node = Node(data)
         p = self.head
@@ -23,12 +31,14 @@ class LinkList:
                 p = p.next
             p.next = node
 
+#Method to print nodes of linklist
     def print_list(self):
         node = self.head
         while node:
             print(node.data + " ")
             node = node.next
 
+#Main method
 def main():
     l = LinkList()
     val = input("enter the node value: \n")
